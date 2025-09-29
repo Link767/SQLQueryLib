6ac760269b7a7ab9dc79b88896965875a16e8934
Buddha <aleisandrj@gmail.com>
2025-09-29 22:30:54 +0300

    Insert

---
+++ SqlLib/SqlLib/SqlManager.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.SqlClient;
+namespace SqlLib
+{
+    public class SqlManager
+    {
+        private string ConnectionString {  get; set; }
+        public SqlManager(string ConnectionString)
+        {
+            this.ConnectionString = ConnectionString;
+        }
+
+        public void DBInf(SqlConnection conn)
+        {
+            //Open
+            Console.WriteLine("Connection String: " + conn.ConnectionString);
+            Console.WriteLine("State: " + conn.State);
+            Console.WriteLine("Database: " + conn.Database);
+            Console.WriteLine("DataSource: " + conn.DataSource);
+            Console.WriteLine("ServerVersion: " + conn.ServerVersion + "\n");
+
+        }
+        public string Insert(string table, string columns, string data)
+        {
+            //string query = "INSERT INTO [User] (Login, Pass) VALUES ('Tom', 36)";
+            string status; 
+            string query = $"INSERT INTO {table} ({columns}) VALUES ({data})";
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                SqlCommand command = conn.CreateCommand();
+                SqlTransaction transaction = conn.BeginTransaction();
+
+                command.Connection = conn;
+                command.Transaction = transaction;
+
+               try
+               {
+                    command.CommandText = query;
+                    command.ExecuteNonQuery(); // Выполнить запрос
+
+                    transaction.Commit();
+                    status = "Commit Done";
+               }
+               catch (Exception ex)
+               {
+                    transaction.Rollback();
+                    status = $"err: {ex.GetType()}";
+               }
+
+            }
+            return status;
+        }
+    }
+}
---
+++ SqlLib/SqlLibTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using SqlLib;
+namespace SqlLibTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connStr = "Server=(localdb)\\MSSQLLocalDB;Database=BMDB;Trusted_Connection=True;";
+            SqlManager sqlManager = new SqlManager(connStr);
+            string table = "[User]";
+            string column = "Login, Pass, IdSettings, IdRole";
+            string data = "'login', 'pass', 1, 1";
+            string status =  sqlManager.Insert(table, column, data);
+
+            Console.WriteLine(status);
+        }
+    }
+}
